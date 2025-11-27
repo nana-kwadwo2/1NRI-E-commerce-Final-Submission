bed86e432be61ed9c2e5f2a6864b4b7fa511972f
nana-kwadwo <nkon290@gmail.com>
2025-11-26 21:05:16 -0800

    hi

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,7 +5,7 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Import the supabase client like this:
+// Import the supabase client like this: hello
 // import { supabase } from "@/integrations/supabase/client";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
